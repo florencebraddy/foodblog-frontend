998ff9e0f3fc40dc30cfbc79f254bc8341830736
Florence Braddy <florencebraddy@hotmail.com>
2020-07-23 22:35:50 -0400

    created amplify bucket for recipestorage

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 import Amplify from "aws-amplify";
 import awsexports from "./aws-exports";
 Amplify.configure(awsexports);
@@ -9,7 +9,5 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-
